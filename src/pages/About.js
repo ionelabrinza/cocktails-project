ef2113630e1277247f8e76fa3a555520df86832f
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-20 20:28:45 -0500

    about and error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus nisi
+        saepe modi dignissimos error sint placeat ea suscipit, fugiat tenetur
+        incidunt accusamus architecto omnis sequi facilis quis eveniet ut
+        aliquam recusandae iusto corporis atque iure? Aperiam numquam obcaecati
+        deserunt! Ab.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
